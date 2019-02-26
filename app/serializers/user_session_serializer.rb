80e0c21e53710eac921ae24b7916ed3d50923589
Thompson Plyler <nealok.g@gmail.com>
2019-02-26 18:02:09 -0500

    More back end.

--- app/serializers/user_session_serializer.rb
+++ app/serializers/user_session_serializer.rb
@@ -1,5 +1,5 @@
 class UserSessionSerializer < ActiveModel::Serializer
-    attributes :id, :session, :completed, :user, :workouts
+    attributes :id, :completed, :user_id, :session_id, :session, :user, :session_workouts
     
     def user
         {user_id: self.object.user.id,
@@ -14,13 +14,31 @@ class UserSessionSerializer < ActiveModel::Serializer
         } 
     end
 
-    def workouts
-        session_workouts = self.object.session.session_workouts.map{|session_workout| session_workout}
+    def session_workouts
+        workouts_sort = self.object.session.session_workouts.sort_by{|workout|workout.order} 
+        
+        session_exercises = workouts_sort.map do |session_workout|
+            session_workout.workout.exercises
+        end
+
+        workouts = workouts_sort.map do |session_workout| 
+            {component: session_workout.workout.name,
+                exercises: session_workout.workout.workout_exercises}
+                # exercises: session_workout.workout.workout_exercises}
+        end
 
 
         {
-            workouts: session_workouts
+            workouts: workouts
         }
+    end
+
+    def exercises
+        workouts_sort = self.object.session.session_workouts.sort_by{|workout|workout.order}
+        
+        exercises = workouts_sort.map do |session_workout|
+            {exercises: session_workout.workout.workout_exercises}
+        end
     end
 
 end
